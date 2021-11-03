b706087e260330d9d78785ac794d80a06f4668fc
deejay0825 <plmnj2003@naver.com>
2021-11-03 20:31:55 +0900

    Initial Commit

---
+++ MintNote_V1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MintNote_V1.0
+//
+//  Created by 서동진 on 2021/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MintNote_V1.0/MintNote_V1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MintNote_V1_0App.swift
+//  MintNote_V1.0
+//
+//  Created by 서동진 on 2021/11/03.
+//
+
+import SwiftUI
+
+@main
+struct MintNote_V1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
